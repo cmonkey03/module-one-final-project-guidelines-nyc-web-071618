2cc71973a069902659bf2b6bfd1c62d693a04863
github username <github email address>
2018-08-03 06:00:39 -0400

    added most and least talkative race class methods to Chapter model

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -24,4 +24,39 @@ class Chapter < ActiveRecord::Base
     end
   end
 
+  def self.populated_race_count_hash(film)
+    film_chapters = Chapter.all.where(:film => film)
+    film_wordcounts = film_chapters.map {|chapter| chapter.wordcounts}.flatten
+    film_races = film_wordcounts.map {|wordcount| wordcount.character.race }.uniq
+
+
+    race_count = {}
+    film_races.each do |race|
+      race_count[race] = 0
+    end
+
+    film_wordcounts.each {|wordcount| race_count[wordcount.character.race] += wordcount.count }
+    race_count
+
+  end
+
+  def self.most_talkative_race_in_film(film)
+    Chapter.populated_race_count_hash(film).sort_by { |_key, value| value }.to_h.keys.last
+  end
+
+  def self.least_talkative_race_in_film(film)
+    Chapter.populated_race_count_hash(film).sort_by { |_key, value| value }.to_h.keys.first
+  end
+    # film_unique_races.each do |race|
+    #   race_count[race.to_sym] = 0
+    # end
+    #
+    # film_races.each do |race|
+    #   race_count[race.to_sym] +=
+    # end
+
+    # race_count.sort_by { |_key, value| value }.to_h.keys.last
+
+
+
 end # end of Chapter class
